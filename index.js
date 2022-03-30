8dbe0c04fbc12cdf7db669beaf4d1716e25514a8
henteko <henteko07@gmail.com>
2022-03-30 12:50:21 +0000

    cardのデザインを作成

---
+++ index.js
@@ -0,0 +1,7 @@
+html2canvas(document.querySelector(".card")).then(canvas => {
+  // download card
+  // let link = document.createElement("a");
+  // link.href = canvas.toDataURL("image/png");
+  // link.download = "card.png";
+  // link.click();
+});
