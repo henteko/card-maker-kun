150b6f28f7573b7074bf06de80ccf54af9aea865
henteko <henteko07@gmail.com>
2022-04-03 05:05:35 +0000

    背景を透過させた

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector("#export").addEventListener("click", () => {
-  html2canvas(document.querySelector(".card")).then(canvas => {
+  html2canvas(document.querySelector(".card"), { backgroundColor: null }).then(canvas => {
     let link = document.createElement("a");
     link.href = canvas.toDataURL("image/png");
     link.download = "card.png";
