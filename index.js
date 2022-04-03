354e3691ab20ed914224a829ab12789740b78ada
henteko <henteko07@gmail.com>
2022-04-03 06:21:12 +0000

    文字入力できるようにした

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 document.querySelector("#export").addEventListener("click", () => {
   html2canvas(document.querySelector(".card"), { backgroundColor: null }).then(canvas => {
-    let link = document.createElement("a");
+    const link = document.createElement("a");
     link.href = canvas.toDataURL("image/png");
     link.download = "card.png";
     link.click();
   });
+});
+
+document.querySelector("#top-text").addEventListener("input", (event) => {
+  const element = document.querySelector(".card-contents > .top");
+  element.innerHTML = event.target.value; 
+});
+
+document.querySelector("#body-text").addEventListener("input", (event) => {
+  const element = document.querySelector(".card-contents > .body");
+  element.innerHTML = event.target.value.replace(/\n/g,'<br>'); 
+});
+
+document.querySelector("#bottom-text").addEventListener("input", (event) => {
+  const element = document.querySelector(".card-contents > .bottom");
+  element.innerHTML = event.target.value; 
 });
